45c7c15dcf94d6aa2348d7eaf756e265dd991813
Gabriel <89980949+emptyvoxel@users.noreply.github.com>
2026-01-02 15:39:01 -0300

    Implement the HTTP Server

--- rmctph/src/main.rs
+++ rmctph/src/main.rs
@@ -1,5 +1,9 @@
 use tiny_http::{Method, Response, Server};
 
+fn set_volume (volume: u8) {
+    println!("volume set to {}", volume);
+}
+
 fn main() {
     let ip_addr: &str = "0.0.0.0:5000";
     let server = Server::http(ip_addr).unwrap();
@@ -15,14 +19,29 @@ fn main() {
 
             println!("{}", body);
 
+            if let Some(level) = body.strip_prefix("level=") {
+                if let Ok(volume) = level.parse::<u8>() {
+                    if volume <= 100 {
+                        set_volume(volume);
+
+                        let _ = request.respond(
+                            Response::from_string("OK")
+                                .with_status_code(200)
+                        );
+
+                        continue;
+                    }
+                }
+            }
+
             let _ = request.respond(
-                Response::from_string(body)
-                    .with_status_code(200)
+                Response::from_string("Bad request")
+                    .with_status_code(400)
             );
         } else {
             let _ = request.respond(
                 Response::from_string("Something Went Wrong")
-                    .with_status_code(400)
+                    .with_status_code(500)
             );
         }
     }

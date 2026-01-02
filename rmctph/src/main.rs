36b5ec055cd43bbe2e2af25f84440a5c12aa40c8
Gabriel <89980949+emptyvoxel@users.noreply.github.com>
2026-01-02 15:10:08 -0300

    Create a simple echo server

--- rmctph/src/main.rs
+++ rmctph/src/main.rs
@@ -1,3 +1,29 @@
+use tiny_http::{Method, Response, Server};
+
 fn main() {
-    println!("F*ck RMCTPH");
+    let ip_addr: &str = "0.0.0.0:5000";
+    let server = Server::http(ip_addr).unwrap();
+
+    for mut request in server.incoming_requests() {
+        if request.method() == &Method::Post && request.url() == "/volume" {
+            let mut body = String::new();
+
+            request
+                .as_reader()
+                .read_to_string(&mut body)
+                .unwrap();
+
+            println!("{}", body);
+
+            let _ = request.respond(
+                Response::from_string(body)
+                    .with_status_code(200)
+            );
+        } else {
+            let _ = request.respond(
+                Response::from_string("Something Went Wrong")
+                    .with_status_code(400)
+            );
+        }
+    }
 }
